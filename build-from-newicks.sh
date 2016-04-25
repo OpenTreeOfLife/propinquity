0858b541bbd3a9148d39a38eb59a30ca82221534
Benjamin Redelings <benjamin.redelings@gmail.com>
2016-04-25 18:45:32 -0400

    Partially fix build-from-newicks.sh

--- build-from-newicks.sh
+++ build-from-newicks.sh
@@ -11,11 +11,21 @@ then
     echo "build-from-newicks.sh: expecting the second argument to be a file listing the path to the newick files"
     exit 1
 fi
+PEYOTL_ROOT=$(bin/config_checker.py opentree.peyotl config ~/.opentree)
 if test -z "$PEYOTL_ROOT"
 then
     echo "currently you need to have PEYOTL_ROOT in your env set to your local clone of https://github.com/mtholder/peyotl.git"
     exit 1
 fi
+
+echo "[opentree]" > config
+echo "ott=${ottdir}" >> config
+echo >> config
+echo "[synthesis]" >> config
+echo "collections = " >> config
+echo >> config
+echo "[taxonomy]" >> config
+echo "cleaning_flags = " >> config
 
 touch phylo_input/rank_collection.json
 
@@ -39,5 +49,24 @@ do
     python "${PEYOTL_ROOT}/scripts/nexson/propinquity_newick_to_nexson.py" "--ids=${tree_id}" $i > phylo_snapshot/"${stem}.json"
 done
 
+echo > phylo_input/collections.txt
+echo > phylo_input/rank_collection.json
+echo > phylo_input/study_tree_pairs.txt
+
+echo > phylo_snapshot/concrete_rank_collection.json
+echo > cleaned_phylo/needs_updating.txt
+echo > cleaned_phylo/root_ott_id.txt
+echo > cleaned_phylo/cleaning_flags.txt
+
 echo 0 > phylo_snapshot/git_shas.txt
+echo 0 > cleaned_ott/ott_version.txt
+echo > cleaned_ott/cleaning_flags.txt
+echo > cleaned_ott/root_ott_id.txt
+echo > cleaned_phylo/needs_updating.txt
+cp $phyloranking cleaned_phylo/phylo_inputs_cleaned.txt
+#cp $phyloranking phylo_input/study_tree_pairs.txt
+#sed 's/\.tre/\.json/' $phyloranking > cleaned_phylo/needs_updating.txt
+cp $phyloranking exemplified_phylo/args.txt
+
+echo > cleaned_phylo/root_ott_id.txt
 bin/fake-collection.py $(cat "${phyloranking}") > phylo_snapshot/concrete_rank_collection.json
