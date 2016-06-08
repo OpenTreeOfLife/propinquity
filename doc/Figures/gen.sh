f45a65da3fc485d3afaaf853a5636e536efca56a
Benjamin Redelings <benjamin.redelings@gmail.com>
2016-06-08 16:36:14 -0400

    Generate SVGs.

--- doc/Figures/gen.sh
+++ doc/Figures/gen.sh
@@ -8,7 +8,7 @@ function draw {
     mkdir -p $(dirname $file)
     otc-relabel-tree --taxonomy=$OTT $EXAMPLE/$file --format-tax=%N > $file
     $DRAWTREE --input=$file --output=$file.svg \
-	      --edge.width=3 "$@"
+	      --edge.width=3 "$@" --format=svg
 
 }
 
@@ -19,3 +19,6 @@ draw exemplified_phylo/ex_2@tree1.tre
 draw exemplified_phylo/ex_2@tree2.tre
 draw exemplified_phylo/taxonomy.tre --show.node.label=TRUE
 draw grafted_solution/grafted_solution_ottnames.tre --show.node.label=TRUE
+draw subproblems/14/1.tre
+draw subproblems/14/2.tre
+draw subproblems/14/3.tre
