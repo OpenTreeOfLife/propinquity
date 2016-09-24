230c4e8d8f3856857489f81a3402f97773202d10
Benjamin Redelings <benjamin.redelings@gmail.com>
2016-09-24 14:49:56 -0400

    Update ms.

--- doc/paper1-submission/copy_figures.sh
+++ doc/paper1-submission/copy_figures.sh
@@ -4,15 +4,16 @@ FIGURES=$(grep '{.*/' paper.tex | sed 's|.*{\(.*/.*\)}.*|\1|'| sed 's|\\lyxdot |
 for figure in $FIGURES ; do
     filename="../${figure}"
     new_filename=$(echo $figure | sed 's|/|__|g')
-    new_filename=${new_filename%%.pdf}.pdf
     echo "\n\n'$figure' -> ${new_filename}"
     # If the lyx svg-to-pdf command is correctly set to do "--export-area-drawing", then regenerating pdfs is unnecessary.
+    if [ -f "${filename}" ] ; then
+	ln -s "$filename" "$new_filename"
+    fi
     if [ -f "${filename}.svg" ] ; then
-	filename="${filename}.svg"
-	inkscape "$filename" "--export-pdf=${new_filename}" --export-area-drawing
+	ln -s "${filename}.svg" "${new_filename}.svg"
+	inkscape "${new_filename}.svg" "--export-pdf=${new_filename}.pdf" --export-area-drawing
     else
-	filename="${filename%.pdf}.pdf"
-	cp "${filename}" "${new_filename}"
+	ln -s "${filename%.pdf}.pdf" "${new_filename%.pdf}.pdf"
     fi
 done
 
