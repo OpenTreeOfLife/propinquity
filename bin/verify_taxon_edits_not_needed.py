4fd92496c15c1c82c09321ee6833a0acb8969e65
Mark T. Holder <mtholder@gmail.com>
2019-05-09 11:09:16 -0500

    allow 1 arg version

--- bin/verify_taxon_edits_not_needed.py
+++ bin/verify_taxon_edits_not_needed.py
@@ -3,9 +3,14 @@ import codecs
 import json
 import sys
 import os
-jfp = sys.argv[1]
-with codecs.open(jfp, mode='r', encoding='utf-8') as jinp:
-    jout = json.load(jinp)
+usage = 'Expecting 1 argument (JSON edits) or 3: JSON, src_ott_tree, dest_ott_tree\n'
+try:
+    jfp = sys.argv[1]
+    with codecs.open(jfp, mode='r', encoding='utf-8') as jinp:
+        jout = json.load(jinp)
+except:
+    sys.stderr.write(usage)
+    raise
 if "edits" in jout:
     sys.exit('''Taxonomic changes need to be made in your version of OTT to move extinct taxa higher in the taxonomy!
 
@@ -14,5 +19,10 @@ Use:
 to create a modified version of OTT, then set that to be your OTT path for propinquity and rerun.
 
 '''.format(os.path.abspath(os.curdir), os.path.abspath(jfp)))
-from shutil import copyfile
-copyfile(sys.argv[2], sys.argv[3])
+if len(sys.argv) > 2:
+    try:
+        from shutil import copyfile
+        copyfile(sys.argv[2], sys.argv[3])
+    except:
+        sys.stderr.write(usage)
+        raise
