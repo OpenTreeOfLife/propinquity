19d20607894c6bf7f4bb062c5347c35aeafbb261
Mark T. Holder <mtholder@gmail.com>
2019-10-11 14:14:11 -0500

    refactored tarball script into a top level and tree-only script

--- bin/make_tarballs.sh
+++ bin/make_tarballs.sh
@@ -12,6 +12,7 @@
 #   outputs, including `*.html` files
 
 # command line args
+propinquity_bin_dir="`dirname $0`"
 if test -z $1
 then
   echo "expecting first argument to be an output directory"
@@ -47,36 +48,9 @@ if [ "${preoutputdir}" != "${outputdir}" ] ; then
     cp -a ${preoutputdir} ${outputdir}
 fi
 
-# first (smaller) tarball
-tar_dir1="${treeid}_tree"
-mkdir -p $tar_dir1
-mkdir -p $tar_dir1/labelled_supertree
-mkdir -p $tar_dir1/grafted_solution
-cp -p $outputdir/labelled_supertree/labelled_supertree.tre $tar_dir1/labelled_supertree/
-cp -p $outputdir/labelled_supertree/labelled_supertree_ottnames.tre $tar_dir1/labelled_supertree/
-cp -p $outputdir/grafted_solution/grafted_solution.tre $tar_dir1/grafted_solution/
-cp -p $outputdir/grafted_solution/grafted_solution_ottnames.tre $tar_dir1/grafted_solution/
-cp -p $outputdir/annotated_supertree/annotations.json $tar_dir1/
 
-# readme file
-echo 'printing readme file'
-files_url="http://files.opentreeoflife.org/synthesis/$treeid/output"
-touch $tar_dir1/README.md
-printf "[Release notes for version %s](https://github.com/OpenTreeOfLife/germinator/blob/release_notes/doc/ot-synthesis-v%s.md)\n" $version > $tar_dir1/README.md
-printf "\nContents of \`%s_tree.tgz\`" $treeid >> $tar_dir1/README.md
-printf "\n* \`output/annotated_supertree/annotations.json\` - synthetic tree annotations; see [docs](%s/annotated_supertree/index.html) for explanation" $files_url >> $tar_dir1/README.md
-printf "\n* \`output/labelled_supertree/\` - see [docs](%s/labelled_supertree/index.html) for explanation; contains:" $files_url >> $tar_dir1/README.md
-printf "\n   * \`labelled_supertree.tre\` - full synthetic tree; labels are ottids" >> $tar_dir1/README.md
-printf "\n   * \`labelled_supertree_ottnames.tre\` - full synthetic tree; labels are name_ottid" >> $tar_dir1/README.md
-printf "\n* \`output/grafted_solution\` - see [docs](%s/grafted-solution/index.html) for explanation; contains:" $files_url >> $tar_dir1/README.md
-printf "\n   * \`grafted_solution.tre\` - synthetic tree without taxonomy-only outputs; labels as ottids" >> $tar_dir1/README.md
-printf "\n   * \`grafted_solution_ottnames.tre\` - synthetic tree without taxonomy-only outputs; labels are name_ottid" >> $tar_dir1/README.md
+bash "${propinquity_bin_dir}/make_tree_tarball.sh" "${outputdir}" "${treeid}" "${version}"
 
-# tar these babies up
-echo 'creating full archive'
-tar -czf $tar_dir1.tgz $tar_dir1
-
-echo 'creating tree-only archive'
 # all synthesis outputs
 tar -czf ${outputdir}_output.tgz $outputdir
 
