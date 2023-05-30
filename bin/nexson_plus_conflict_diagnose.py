bac6922d596160b571cea45e9b5752219ab46645
Mark T. Holder <mtholder@gmail.com>
2023-05-30 16:55:51 -0500

    better

--- bin/nexson_plus_conflict_diagnose.py
+++ bin/nexson_plus_conflict_diagnose.py
@@ -27,6 +27,36 @@ def main(nexson_fp, conflict_call_fp, tree_id):
             assert len(w) == len(wn)
             assert len(w) > 0
             nd_2_conf[nd_id] = (len(w), w, wn)
+
+    root_id = tree["^ot:rootNodeId"]
+    assert root_id == tree["^ot:specifiedRoot"]
+    ebsi = tree["edgeBySourceId"]
+    indent_num = 0
+    next_to_deal_with = (indent_num, root_id)
+    deferred_stack = []
+    while next_to_deal_with:
+        next_tdw = next_to_deal_with
+        idn, nd_id = next_tdw
+        conflict = nd_2_conf.get(nd_id, '')
+        istr = ' '*idn
+        print(f"{istr}{nd_id} {conflict}")
+        ebsi_el = ebsi.get(nd_id)
+        if ebsi_el is None:
+            if deferred_stack:
+                next_to_deal_with = deferred_stack.pop(0)
+            else:
+                break
+            continue
+        nn = None
+        for edge_id, eblob in ebsi_el.items():
+            assert eblob["@source"] == nd_id
+            tid = eblob["@target"]
+            if nn is None:
+                nn = tid
+                next_to_deal_with = (1+idn, nn)
+            else:
+                deferred_stack.insert(0,(1+idn, tid))
+
     
 
 
