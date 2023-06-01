c6a51dffdead00505ecde7dad337e64456f5fbbb
Mark T. Holder <mtholder@gmail.com>
2023-05-31 20:57:29 -0500

    some improvements, but I probably should have renamed this script

--- bin/nexson_plus_conflict_diagnose.py
+++ bin/nexson_plus_conflict_diagnose.py
@@ -11,21 +11,37 @@ def ntw_preorder(ntw):
     curr_nd_id = ntw.root_node_id
     nd_id_stack = []
     taboo_set = set()
+    id_to_par_id = {}
     while True:
         taboo_set.add(curr_nd_id)
         curr_nd = nbi[curr_nd_id]
-        yield curr_nd
         ebsi_el = ebs.get(curr_nd_id)
         pcni = curr_nd_id
         c_added = False
         curr_nd['children'] = []
+        otu_id = curr_nd.get('@otu')
+        if otu_id:
+            otu_obj = ntw.otus[otu_id]
+            ott_id = otu_obj.get('^ot:ottId')
+            curr_nd['ott_id'] = ott_id
+            if ott_id is not None:
+                curr_nd['ott_name'] = otu_obj['^ot:ottTaxonName']
+        else:
+            curr_nd['ott_id'] = None
+        par_id = id_to_par_id.get(curr_nd_id)
+        if par_id is None:
+            assert curr_nd_id == ntw.root_node_id
+        curr_nd['par_id'] = par_id
+        yield curr_nd
         c = curr_nd['children']
         if ebsi_el:
             for idx, tup in enumerate(ebsi_el.items()):
                 edge_id, eblob = tup
                 assert eblob["@source"] == pcni
                 tid = eblob["@target"]
-                c.append(nbi[tid])
+                c.append(tid)
+                assert tid not in id_to_par_id
+                id_to_par_id[tid] = pcni
                 if tid not in taboo_set:
                     if not c_added:
                         curr_nd_id = tid
@@ -36,12 +52,109 @@ def ntw_preorder(ntw):
             if not nd_id_stack:
                 break
             curr_nd_id = nd_id_stack.pop()
-            
 
+def common_anc_prefix(lap):
+    non_none = [i for i in lap if i is not None]
+    if len(non_none) == 0:
+        return None
+    if len(non_none) == 1:
+        return non_none[0]
+    fnn = non_none[0]
+    rest = non_none[1:]
+    common = []
+    idx = 0
+    while True:
+        try:
+            fid = fnn[idx]
+            for nn in rest:
+                if nn[idx] != fid:
+                    return tuple(common)
+            common.append(fid)
+            idx += 1
+        except IndexError:
+            return tuple(common)
 
+def get_parent(nd, nbi):
+    par_id = nd['par_id']
+    if par_id is None:
+        return None
+    return nbi[par_id]
 
+def get_par_and_grandparent(nd, nbi):
+    par = get_parent(nd, nbi)
+    return (None, None) if par is None else (par, get_parent(par, nbi))
 
+def calc_len_par_anc_list_skipping(curr_nd, par_nd, gpar_nd, nbi):
+    curr_nd_id = curr_nd["@id"]
+    sibs = [i for i in par_nd['children'] if i != curr_nd_id]
+    pmai = common_anc_prefix([nbi[i]['anc_tup'] for i in sibs])
+    real_par_ai = par_nd['anc_tup']
+    if (not pmai) or (not real_par_ai):
+        par_shortened = 0
+    else:
+        par_shortened = len(pmai) - len(real_par_ai)
+    assert par_shortened >= 0
+    if gpar_nd is None:
+        return par_shortened, 0
+    par_nd_id = par_nd["@id"]
+    aunts = [i for i in gpar_nd['children'] if i != par_nd_id]
+    aunts_ai = [nbi[i]['anc_tup'] for i in aunts]
+    par_gen_ai = aunts_ai + [pmai]
+    gpmai = common_anc_prefix(par_gen_ai)
+    real_gpar_ai = gpar_nd['anc_tup']
+    if (not gpmai) or (not real_gpar_ai):
+        gpar_shortened = 0
+    else:
+        gpar_shortened = len(gpmai) - len(real_gpar_ai)
+    assert gpar_shortened >= 0
+    return par_shortened, gpar_shortened
+    
 
+def detect_problem_mapping(ntw, nex_preorder, to_tax_nd):
+    nbi = ntw._node_by_id
+    for nd in reversed(nex_preorder):
+        children = nd['children']
+        if children:
+            nd['anc_tup'] = common_anc_prefix([nbi[i]['anc_tup'] for i in children])
+            # print(f"{nd['@id']} -> {nd['anc_tup']}")
+        else:
+            ott_id = nd.get('ott_id')
+            nd['anc_tup'] = None
+            if ott_id is None:
+                sys.stderr.write(f'  Node {nd["@id"]} is UNMAPPED\n')
+            else:
+                tax_nd = to_tax_nd.get(ott_id)
+                if tax_nd is None:
+                    sys.stderr.write(f'  Node {nd["@id"]} is mapped to a taxon {ott_id} not in the induced taxonomy\n')
+                else:
+                    nd['anc_tup'] = tax_nd.anc_tup
+
+    sortable = []
+    for nd in reversed(nex_preorder):
+        score_list = [0, 0, 0, 0]
+        par, gpar = get_par_and_grandparent(nd, nbi)
+        if par is None:
+            continue
+        nat = nd['anc_tup']
+        if nat is not None:
+            pat = par['anc_tup']
+            if pat is None:
+                items_lost = len(nat)
+            else:
+                items_lost = len(nat) - len(pat)
+            assert items_lost >= 0
+            score_list[1] = items_lost
+            par_missing, gpar_missing = calc_len_par_anc_list_skipping(nd, par, gpar, nbi)
+            score_list[2] = par_missing
+            score_list[3] = gpar_missing
+        score_list[0] = sum(score_list[1:])
+        nd_id = nd["@id"]
+        sortable_val = tuple(score_list + [nd_id, nd])
+        sortable.append(sortable_val)
+    sortable.sort(reverse=True)
+    return sortable
+
+          
 
 def main(nexson_fp, conflict_call_fp, tree_id, taxonomy_fp, root_at_id=None):
     nexson_blob = read_as_json(nexson_fp)
@@ -50,13 +163,8 @@ def main(nexson_fp, conflict_call_fp, tree_id, taxonomy_fp, root_at_id=None):
     ntw.prune_unmapped_leaves()
     ott_id_to_sortable_list = ntw.group_and_sort_leaves_by_ott_id()
     nex_preorder = [i for i in ntw_preorder(ntw)]
-    print(f"len(ott_id_to_sortable_list) = {len(ott_id_to_sortable_list)}")
     
-
-    # tax_tree.print_plot(show_internal_node_labels=True)
-    print(f"ntw.root_node_id = {ntw.root_node_id}")
-    sys.exit('early\n')
-
+    
     tax_tree = Tree.get_from_path(taxonomy_fp, schema="newick")
     for nd in tax_tree.postorder_node_iter():
         if not nd.label:
@@ -69,17 +177,26 @@ def main(nexson_fp, conflict_call_fp, tree_id, taxonomy_fp, root_at_id=None):
                 nd.leaf_des.update(c.leaf_des)
         else:
             nd.leaf_des = set([nd.label])
-        print(nd.label)
+    
+    ott_id2_tax_nd = {}
     for nd in tax_tree.preorder_node_iter():
+        ott_id2_tax_nd[nd.label] = nd
         if nd is tax_tree.seed_node:
-            nd.anc_id = tuple()
+            nd.anc_tup = tuple()
             continue
         par = nd.parent_node
-        nd.anc_id = tuple(list(par.anc_id) + [par.label])
-        print(nd.label, nd.anc_id)
-    sys.exit('early\n')
+        nd.anc_tup = tuple(list(par.anc_tup) + [par.label])
+    
+    x = detect_problem_mapping(ntw, nex_preorder, ott_id2_tax_nd)
+    for idx, sv in enumerate(x):
+        print(sv)
+        if idx > 9:
+            break
+    
 
-    with open(conflict_call_fp, "r") as inp:
+    return
+
+    z = """with open(conflict_call_fp, "r") as inp:
         conflict = json.load(inp)
     with open(nexson_fp, "r") as inp:
         nexml = json.load(inp)["nexml"]
@@ -156,7 +273,7 @@ def main(nexson_fp, conflict_call_fp, tree_id, taxonomy_fp, root_at_id=None):
                     next_to_deal_with = (pref, tid, curr_pref + ' │')
                 else:
                     deferred_stack.insert(0,(pref, tid, curr_pref + '  '))
-
+    """
     
 
 
